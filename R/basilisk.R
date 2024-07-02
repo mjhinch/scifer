636e13d0ed69d0439e9f372b0635bb825a9f465c
Matt <mjhinchcliff@gmail.com>
2024-07-02 16:13:21 +0200

    Updated channels for conda environment
    -Added bioconda channel to environment creation

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,7 @@
 #' @importFrom basilisk BasiliskEnvironment
 env <- BasiliskEnvironment(envname="igblast_wrap_basilisk",
                               pkgname = "scifer",
-                              packages=c("python=3.9", "dnaio==1.2.1", "igblast==1.22.0")
+                              packages=c("python=3.9", "dnaio==1.2.1", "igblast==1.22.0"),
+                              channels= "bioconda"
+                           
 )
