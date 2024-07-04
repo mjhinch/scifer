f6dbd151ce271ac0e7b611ffeb7bd09cca468962
Matt <mjhinchcliff@gmail.com>
2024-07-04 13:33:08 +0200

    Updated description & env creation
    -Import Utils added to description file after BiocCheck
    -Updated Channels in BasiliskEnvironment creation to add conda-forge

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,7 +1,10 @@
 #' @importFrom basilisk BasiliskEnvironment
 env <- BasiliskEnvironment(envname="igblast_wrap_basilisk",
                               pkgname = "scifer",
-                              packages=c("python=3.9", "dnaio==1.2.1", "igblast==1.22.0"),
-                              channels= "bioconda"
+                              packages=c("python==3.9.19", 
+                                         "dnaio==1.2.1", 
+                                         "igblast==1.22.0"
+                                         ),
+                              channels= c("bioconda", "conda-forge")
                            
 )
